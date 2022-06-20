aecc78e385ac26f0ce99285e3eb690eda9dad7a0
Alexander Hedberg <alexander.hedberg@mimer.com>
2022-06-20 20:17:10 +0200

    docs(statement-methods): documentation
    Added documentation for executer and param hook methods.

--- mimer_stmt.c
+++ mimer_stmt.c
@@ -57,6 +57,11 @@ end:
     return 1;
 }
 
+/**
+ * @brief Execute a prepared SQL statement.
+ * @param stmt Pointer to the statement structure initialized by pdo_mimer_handle_preparer.
+ * @return 1 for success, 0 for failure.
+ */
 static int pdo_mimer_stmt_executer(pdo_stmt_t *stmt) {
     pdo_mimer_stmt *stmt_handle = stmt->driver_data;
     int32_t return_code;
@@ -84,6 +89,58 @@ static int pdo_mimer_stmt_get_col_data(pdo_stmt_t *stmt, int colno, zval *result
 
 }
 
+/**
+ * @brief Handle bound parameters and columns
+ * @param stmt stmt Pointer to the statement structure initialized by pdo_mimer_handle_preparer.
+ * @param param The structure describing either a statement parameter or a bound column.
+ * @param event_type The type of event to occur for this parameter.
+ * @return 1 for success, 0 for failure.
+ *
+ * @details
+ *  @code <h3>PDO_PARAM_EVT_NORMALIZE</h3> @endcode
+ *      Event triggered during bound parameter registration allowing the driver to normalize the parameter name.
+ *  @code <h3>PDO_PARAM_EVT_ALLOC</h3> @endcode
+ *      Called when PDO allocates the binding. Occurs as part of
+ *      <a href="https://php-legacy-docs.zend.com/manual/php5/en/pdostatement.bindparam.html">PDOStatement::bindParam()</a>,
+ *      <a href="https://php-legacy-docs.zend.com/manual/php5/en/pdostatement.bindvalue.html">PDOStatement::bindValue()</a>
+ *      or as part of an implicit bind when calling
+ *      <a href="https://php-legacy-docs.zend.com/manual/php5/en/pdostatement.execute.html">PDOStatement::execute()</a>.
+ *      This is your opportunity to take some action at this point; drivers that implement native prepared statements
+ *      will typically want to query the parameter information, reconcile the type with that requested by the script,
+ *      allocate an appropriately sized buffer and then bind the parameter to that buffer.
+ *  @code <h3>PDO_PARAM_EVT_FREE</h3> @endcode
+ *      Called once per parameter as part of cleanup. You should release any resources associated with that parameter now.
+ *  @code <h3>PDO_PARAM_EXEC_PRE</h3> @endcode
+ *      Called once for each parameter immediately before calling <code>pdo_mimer_stmt_executer</code>;  take this
+ *      opportunity to make any final adjustments ready for execution. In particular, you should note that variables
+ *      bound via
+ *      <a href="https://php-legacy-docs.zend.com/manual/php5/en/pdostatement.bindparam.html">PDOStatement::bindParam()</a>
+ *      are only legal to touch now, and not any sooner.
+ *  @code <h3>PDO_PARAM_EXEC_POST</h3> @endcode
+ *      Called once for each parameter immediately after calling <code>pdo_mimer_stmt_executer</code>; take this
+ *      opportunity to make any post-execution actions that might be required by your driver.
+ *  @code <h3>PDO_PARAM_FETCH_PRE</h3> @endcode
+ *      Called once for each parameter immediately prior to calling <code>pdo_mimer_stmt_fetch</code>.
+ *  @code <h3>PDO_PARAM_FETCH_POST</h3> @endcode
+ *      Called once for each parameter immediately after calling <code>pdo_mimer_stmt_fetch</code>.
+ *
+ * @remark
+ *      This hook will be called for each bound parameter and bound column in the statement. For <code>ALLOC</code> and
+ *      <code>FREE</code> events, a single call will be made for each parameter or column. The param structure contains
+ *      a <code>driver_data</code> field that the driver can use to store implementation specific
+ *      information about each of the parameters.
+ *      <br><br>
+ *      For all other events, PDO may call you multiple times as the script issues
+ *      <a href="https://php-legacy-docs.zend.com/manual/php5/en/pdostatement.execute.html">PDOStatement::execute()</a>
+ *      and
+ *      <a href="https://php-legacy-docs.zend.com/manual/php5/en/pdostatement.fetch.html">PDOStatement::fetch()</a>
+ *      calls.
+ *
+ * @sa
+ *      <a href="https://php-legacy-docs.zend.com/manual/php5/en/internals2.pdo.implementing">Implementing PDO</a>
+ *      <br>
+ *      <a href="https://www.php.net/manual/en/pdo.constants.php">PHP: Predefined Constants</a>
+ */
 static int pdo_mimer_stmt_param_hook(pdo_stmt_t *stmt, struct pdo_bound_param_data *param, enum pdo_param_event event_type) {
     pdo_mimer_stmt *stmt_handle = stmt->driver_data;
     MimerStatement *statement = &stmt_handle->statement;
@@ -167,6 +224,21 @@ static int pdo_mimer_cursor_closer(pdo_stmt_t *stmt) {
 
 }
 
+/**
+ * @brief The methods implemented by PDO Mimer for statement handling
+ *
+ * @dtor The function called by PDO to destroy a previously constructed statement object.
+ * @executer The function called by PDO to execute the prepared SQL statement in the passed statement object.
+ * @fetcher The function called by PDO to fetch a row from a previously executed statement object.
+ * @describer The function called by PDO to query information about a particular column.
+ * @get_col The function called by PDO to retrieve data from the specified column.
+ * @param_hook The function called by PDO for handling of both bound parameters and bound columns.
+ * @set_attribute The function called by PDO to allow the setting of driver specific attributes for a statement object.
+ * @get_attribute The function called by PDO to allow the retrieval of driver specific attributes for a statement object.
+ * @get_column_meta The function called by PDO to retrieve meta data from the specified column.
+ * @next_rowset The function called by PDO to advance to the next rowset of the batch.
+ * @cursor_closer The function called by PDO to close the active cursor on a statement.
+ */
 const struct pdo_stmt_methods mimer_stmt_methods = {
         pdo_mimer_stmt_dtor,   /* statement destructor method */
         pdo_mimer_stmt_executer,   /* statement executor method */
@@ -180,4 +252,3 @@ const struct pdo_stmt_methods mimer_stmt_methods = {
         pdo_mimer_next_rowset,   /* next statement rowset method */
         pdo_mimer_cursor_closer,   /* statement cursor closer method */
 };
-/* }}} */
